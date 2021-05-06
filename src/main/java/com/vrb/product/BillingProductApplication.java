9da5f212a2bee4826fc08811f80d001a065ca683
Vishal Biradar <vbiradar@salesforce.com>
2021-05-06 23:04:54 +0530

    Inital Commit

---
+++ src/main/java/com/vrb/product/BillingProductApplication.java
@@ -0,0 +1,13 @@
+package com.vrb.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vrb/product/BillingProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vrb.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BillingProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
